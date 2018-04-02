07d59b44c47261c02e0ccb0961a2c824db2968d7
Vishal Sharma <vishaliitr7@gmail.com>
2018-04-02 21:15:41 +0530

    Follow pep8 in models.py

--- steps/app/models.py
+++ steps/app/models.py
@@ -12,60 +12,72 @@ from django.core.validators import MaxValueValidator, MinValueValidator
 # Create your models here.
 
 VIS_TYPES = (
-        ('Me', 'Only Me'),
-        ('All', 'All'),
-        ('INC', 'Incubator'),
-        ('SU', 'Start ups'),
-        ('SUT', 'Start Up Team'),
-        ('INCT', 'Incubator Team'),
-    )
+    ('Me', 'Only Me'),
+    ('All', 'All'),
+    ('INC', 'Incubator'),
+    ('SU', 'Start ups'),
+    ('SUT', 'Start Up Team'),
+    ('INCT', 'Incubator Team'),
+)
 
 STATUS_TYPES = (
-        ('A', 'Approved'),
-        ('P', 'In Progress'),
-        ('R', 'Rejected'),
-        ('S', 'Submitted'),
+    ('A', 'Approved'),
+    ('P', 'In Progress'),
+    ('R', 'Rejected'),
+    ('S', 'Submitted'),
 )
 
 ACCESS_TYPES = (
-        ('A', 'Admin'),
-        ('M', 'Member'),
+    ('A', 'Admin'),
+    ('M', 'Member'),
 )
+
 
 class Tag(models.Model):
     name = models.CharField(max_length=300)
     followers = models.ManyToManyField(User, related_name='tags', blank=True)
 
+
 class UserProfile(models.Model):
-    user = models.OneToOneField(User, related_name='userprofile', on_delete=models.CASCADE)
+    user = models.OneToOneField(
+        User, related_name='userprofile', on_delete=models.CASCADE)
     image = models.ImageField(upload_to='users', blank=True, null=True)
     dob = models.DateField(null=True, blank=True, verbose_name='Date Of Birth')
-    gender = models.CharField(null=True, blank=True, max_length=1, choices=(('F', 'Female'), ('M', 'Male')), verbose_name="gender")
+    gender = models.CharField(null=True, blank=True, max_length=1, choices=(
+        ('F', 'Female'), ('M', 'Male')), verbose_name="gender")
     aadhar_no = models.BigIntegerField(blank=True, null=True)
-    nationality = models.CharField(max_length = 100,blank=True, null=True, default='Indian')
+    nationality = models.CharField(
+        max_length=100, blank=True, null=True, default='Indian')
+
     def get_name(self):
-        return self.user.first_name+' '+self.user.last_name
+        return self.user.first_name + ' ' + self.user.last_name
+
     def __str__(self):
         return str(self.user.username)
+
 
 @receiver(post_save, sender=User)
 def create_user_userProfile(sender, instance, created, **kwargs):
     if created:
         UserProfile.objects.create(user=instance)
 
+
 @receiver(post_save, sender=User)
 def save_user_userProfile(sender, instance, **kwargs):
     instance.userprofile.save()
+
 
 class Contact(models.Model):
     TYPES = (
         ('Mob', 'Mobile No.'),
         ('Email', 'Email'),
     )
-    contact_type = models.CharField(max_length=10, choices=TYPES, verbose_name="Contact Type")
-    user = models.ForeignKey(User, verbose_name="User",related_name='contacts', on_delete=models.CASCADE)
+    contact_type = models.CharField(
+        max_length=10, choices=TYPES, verbose_name="Contact Type")
+    user = models.ForeignKey(User, verbose_name="User",
+                             related_name='contacts', on_delete=models.CASCADE)
     value = models.TextField()
-    visibility = models.CharField(max_length=10, choices=VIS_TYPES, verbose_name="Contact Visibility")
+    visibility = models.CharField()
 
     def __str__(self):
         return self.contact_type + '-' + self.user.username
@@ -85,10 +97,13 @@ class Social(models.Model):
         ('GH', 'Github'),
     )
 
-    social_type = models.CharField(max_length=10, choices=TYPES, verbose_name="Contact Type")
-    user = models.ForeignKey(User, verbose_name="User",related_name='socials', on_delete=models.CASCADE)
+    social_type = models.CharField(
+        max_length=10, choices=TYPES, verbose_name="Contact Type")
+    user = models.ForeignKey(User, verbose_name="User",
+                             related_name='socials', on_delete=models.CASCADE)
     value = models.URLField()
-    visibility = models.CharField(max_length=10, choices=VIS_TYPES, verbose_name="Social Visibility")
+    visibility = models.CharField(
+        max_length=10, choices=VIS_TYPES, verbose_name="Social Visibility")
 
     def __str__(self):
         return self.social_type + '-' + self.user.username
@@ -108,7 +123,8 @@ class Skill(models.Model):
 class Location(models.Model):
     latitude = models.DecimalField(max_digits=9, decimal_places=6)
     longitude = models.DecimalField(max_digits=9, decimal_places=6)
-    latlong = models.CharField(max_length=30,blank=True, null=True)
+    latlong = models.CharField(max_length=30, blank=True, null=True)
+
 
 class Organisation(models.Model):
     TYPES = (
@@ -117,8 +133,9 @@ class Organisation(models.Model):
         ('C', 'Company'),
     )
     location = models.OneToOneField(Location, on_delete=models.CASCADE)
-    org_type = models.CharField(max_length=10, choices=TYPES, verbose_name="Organisation Type")
-    name  = models.CharField(max_length=50)
+    org_type = models.CharField(
+        max_length=10, choices=TYPES, verbose_name="Organisation Type")
+    name = models.CharField(max_length=50)
     logo = models.ImageField(upload_to='logos', blank=True, null=True)
     url = models.URLField(blank=True, null=True)
 
@@ -127,49 +144,56 @@ class Organisation(models.Model):
 
 
 class Experience(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='experiences')
-    org = models.ForeignKey(Organisation, on_delete=models.CASCADE, related_name='experiences')
+    user = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name='experiences')
+    org = models.ForeignKey(
+        Organisation, on_delete=models.CASCADE, related_name='experiences')
     start_date = models.DateField()
     end_date = models.DateField(blank=True, null=True)
     description = models.TextField()
 
     def __str__(self):
-        return '%s' % self.user.name + ' '+ self.org.name
+        return '%s' % self.user.name + ' ' + self.org.name
 
     class Meta:
         ordering = ["user"]
 
 
-
 # models for Startup
 class Startup(models.Model):
-    user  = models.OneToOneField(User,related_name='startup', on_delete=models.CASCADE)
-    name  = models.CharField(max_length=200)
+    user = models.OneToOneField(
+        User, related_name='startup', on_delete=models.CASCADE)
+    name = models.CharField(max_length=200)
     website = models.URLField(blank=True, null=True)
-    short_description = models.CharField(max_length = 300)
-    description = models.TextField(blank=True,null=True)
+    short_description = models.CharField(max_length=300)
+    description = models.TextField(blank=True, null=True)
     request_designation = models.CharField(max_length=200)
-    request_user = models.ForeignKey(User, related_name='startup_request', blank=True,null=True,on_delete=models.SET_NULL)
+    request_user = models.ForeignKey(
+        User, related_name='startup_request', blank=True, null=True, on_delete=models.SET_NULL)
     tags = models.ManyToManyField(Tag, related_name='startups', blank=True)
     location = models.OneToOneField(Location, on_delete=models.CASCADE)
     email = models.EmailField(blank=False)
     status = models.CharField(max_length=10, choices=STATUS_TYPES, default='S')
-    members = models.ManyToManyField(User, related_name='startup_members', through='StartupMember', blank=True)
+    members = models.ManyToManyField(
+        User, related_name='startup_members', through='StartupMember', blank=True)
     is_incubated = models.BooleanField(default=False)
 
 
 class StartupFile(models.Model):
     title = models.CharField(max_length=50)
-    file_added = models.FileField(blank=False,upload_to='media/startups/docs')
-    startup = models.ForeignKey(Startup, on_delete=models.CASCADE )
+    file_added = models.FileField(blank=False, upload_to='media/startups/docs')
+    startup = models.ForeignKey(Startup, on_delete=models.CASCADE)
+
     def __str__(self):
         return self.title
 
 
 class StartupsImage(models.Model):
     title = models.CharField(max_length=50)
-    file_added = models.ImageField(blank=False,upload_to='media/startups/imgs')
-    startup = models.ForeignKey(Startup, on_delete=models.CASCADE )
+    file_added = models.ImageField(
+        blank=False, upload_to='media/startups/imgs')
+    startup = models.ForeignKey(Startup, on_delete=models.CASCADE)
+
     def __str__(self):
         return self.title
 
@@ -177,13 +201,15 @@ class StartupsImage(models.Model):
 class StartupMember(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     startup = models.ForeignKey(Startup, on_delete=models.CASCADE)
-    access_level = models.CharField(max_length=10, default='M', choices=ACCESS_TYPES)
+    access_level = models.CharField(
+        max_length=10, default='M', choices=ACCESS_TYPES)
     role = models.CharField(max_length=100)
     joining_date = models.DateTimeField(auto_now_add=True)
 
 
 class StartupAchievement(models.Model):
-    startup = models.ForeignKey(Startup, related_name='achievements', on_delete=models.CASCADE)
+    startup = models.ForeignKey(
+        Startup, related_name='achievements', on_delete=models.CASCADE)
     value = models.TextField(blank=True)
     title = models.CharField(max_length=255)
 
@@ -200,10 +226,13 @@ class StartupContact(models.Model):
         ('Mob', 'Mobile No.'),
         ('Email', 'Email'),
     )
-    contact_type = models.CharField(max_length=10, choices=TYPES, verbose_name="Contact Type")
-    startup = models.ForeignKey(Startup, related_name='contacts', on_delete=models.CASCADE)
+    contact_type = models.CharField(
+        max_length=10, choices=TYPES, verbose_name="Contact Type")
+    startup = models.ForeignKey(
+        Startup, related_name='contacts', on_delete=models.CASCADE)
     value = models.TextField()
-    visibility = models.CharField(max_length=10, default='All', choices=VIS_TYPES, verbose_name="Contact Visibility")
+    visibility = models.CharField(
+        max_length=10, default='All', choices=VIS_TYPES, verbose_name="Contact Visibility")
 
     def __str__(self):
         return self.contact_type + '-' + self.startup.name
@@ -223,10 +252,13 @@ class StartupSocial(models.Model):
         ('GH', 'Github'),
     )
 
-    social_type = models.CharField(max_length=10, choices=TYPES, verbose_name="Contact Type")
-    startup = models.ForeignKey(Startup, related_name='socials', on_delete=models.CASCADE)
+    social_type = models.CharField(
+        max_length=10, choices=TYPES, verbose_name="Contact Type")
+    startup = models.ForeignKey(
+        Startup, related_name='socials', on_delete=models.CASCADE)
     value = models.URLField()
-    visibility = models.CharField(max_length=10, default='All', choices=VIS_TYPES, verbose_name="Social Visibility")
+    visibility = models.CharField(
+        max_length=10, default='All', choices=VIS_TYPES, verbose_name="Social Visibility")
 
     def __str__(self):
         return self.social_type + '-' + self.startup.name
@@ -235,27 +267,30 @@ class StartupSocial(models.Model):
         verbose_name_plural = "Social Accounts - Startup"
 
 
-
-
-
 # models for Incubator
 class Incubator(models.Model):
-    user  = models.OneToOneField(User,related_name='incubator', on_delete=models.CASCADE)
-    name  = models.CharField(max_length=200)
+    user = models.OneToOneField(
+        User, related_name='incubator', on_delete=models.CASCADE)
+    name = models.CharField(max_length=200)
     website = models.URLField(blank=True, null=True)
-    short_description = models.CharField(max_length = 300)
-    description = models.TextField(blank=True,null=True)
+    short_description = models.CharField(max_length=300)
+    description = models.TextField(blank=True, null=True)
     request_designation = models.CharField(max_length=200)
-    request_user = models.ForeignKey(User, related_name='incubator_request', blank=True,null=True,on_delete=models.SET_NULL)
+    request_user = models.ForeignKey(
+        User, related_name='incubator_request', blank=True, null=True, on_delete=models.SET_NULL)
     tags = models.ManyToManyField(Tag, related_name='incubators', blank=True)
     location = models.OneToOneField(Location, on_delete=models.CASCADE)
     space_info = models.CharField(max_length=30)
     email = models.EmailField(blank=False)
     status = models.CharField(max_length=10, choices=STATUS_TYPES, default='S')
-    members = models.ManyToManyField(User, related_name='incubator_members', through='IncubatorMember', blank=True)
-    followers = models.ManyToManyField(User, related_name='incubator_follows', blank=True)
-    ratings = models.ManyToManyField(User, related_name='rated_incubators',through='IncubatorRating', blank=True)
-    incubated_startup = models.ManyToManyField(Startup, related_name = 'incubators', through='IncubatorStartup', blank=True)
+    members = models.ManyToManyField(
+        User, related_name='incubator_members', through='IncubatorMember', blank=True)
+    followers = models.ManyToManyField(
+        User, related_name='incubator_follows', blank=True)
+    ratings = models.ManyToManyField(
+        User, related_name='rated_incubators', through='IncubatorRating', blank=True)
+    incubated_startup = models.ManyToManyField(
+        Startup, related_name='incubators', through='IncubatorStartup', blank=True)
 
 
 class IncubatorRating(models.Model):
@@ -283,7 +318,8 @@ class IncubatorStartup(models.Model):
 class IncubatorMember(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     incubator = models.ForeignKey(Incubator, on_delete=models.CASCADE)
-    access_level = models.CharField(max_length=10, default='M', choices=ACCESS_TYPES)
+    access_level = models.CharField(
+        max_length=10, default='M', choices=ACCESS_TYPES)
     role = models.CharField(max_length=100)
     joining_date = models.DateTimeField(auto_now_add=True)
 
@@ -293,10 +329,13 @@ class IncubatorContact(models.Model):
         ('Mob', 'Mobile No.'),
         ('Email', 'Email'),
     )
-    contact_type = models.CharField(max_length=10, choices=TYPES, verbose_name="Contact Type")
-    incubator = models.ForeignKey(Incubator, related_name='contacts', on_delete=models.CASCADE)
+    contact_type = models.CharField(
+        max_length=10, choices=TYPES, verbose_name="Contact Type")
+    incubator = models.ForeignKey(
+        Incubator, related_name='contacts', on_delete=models.CASCADE)
     value = models.TextField()
-    visibility = models.CharField(max_length=10, default='All', choices=VIS_TYPES, verbose_name="Contact Visibility")
+    visibility = models.CharField(
+        max_length=10, default='All', choices=VIS_TYPES, verbose_name="Contact Visibility")
 
     def __str__(self):
         return self.contact_type + '-' + self.incubator.name
@@ -316,11 +355,13 @@ class IncubatorSocial(models.Model):
         ('GH', 'Github'),
     )
 
-
-    social_type = models.CharField(max_length=10, choices=TYPES, verbose_name="Contact Type")
-    incubator = models.ForeignKey(Incubator, related_name='socials', on_delete=models.CASCADE)
+    social_type = models.CharField(
+        max_length=10, choices=TYPES, verbose_name="Contact Type")
+    incubator = models.ForeignKey(
+        Incubator, related_name='socials', on_delete=models.CASCADE)
     value = models.URLField()
-    visibility = models.CharField(max_length=10, default='All', choices=VIS_TYPES, verbose_name="Social Visibility")
+    visibility = models.CharField(
+        max_length=10, default='All', choices=VIS_TYPES, verbose_name="Social Visibility")
 
     def __str__(self):
         return self.social_type + '-' + self.incubator.name
@@ -330,7 +371,8 @@ class IncubatorSocial(models.Model):
 
 
 class IncubatorAchievement(models.Model):
-    incubator = models.ForeignKey(Incubator, related_name='achievements', on_delete=models.CASCADE)
+    incubator = models.ForeignKey(
+        Incubator, related_name='achievements', on_delete=models.CASCADE)
     value = models.TextField(blank=True)
     title = models.CharField(max_length=255)
 
@@ -344,16 +386,20 @@ class IncubatorAchievement(models.Model):
 
 class IncubatorFile(models.Model):
     title = models.CharField(max_length=50)
-    file_added = models.FileField(blank=False,upload_to='media/incubators/docs')
-    incubator = models.ForeignKey(Incubator, on_delete=models.CASCADE )
+    file_added = models.FileField(
+        blank=False, upload_to='media/incubators/docs')
+    incubator = models.ForeignKey(Incubator, on_delete=models.CASCADE)
+
     def __str__(self):
         return self.title
 
 
 class IncubatorImage(models.Model):
     title = models.CharField(max_length=50)
-    file_added = models.ImageField(blank=False,upload_to='media/incubators/imgs')
-    incubator = models.ForeignKey(Incubator, on_delete=models.CASCADE )
+    file_added = models.ImageField(
+        blank=False, upload_to='media/incubators/imgs')
+    incubator = models.ForeignKey(Incubator, on_delete=models.CASCADE)
+
     def __str__(self):
         return self.title
 
@@ -363,14 +409,16 @@ class IncubatorPost(models.Model):
         ('P', 'Post'),
         ('C', 'Competition'),
     )
-    incubator = models.ForeignKey(Incubator, related_name='posts', on_delete=models.CASCADE)
+    incubator = models.ForeignKey(
+        Incubator, related_name='posts', on_delete=models.CASCADE)
     value = models.TextField(blank=True)
     title = models.CharField(max_length=255)
     added_by = models.ForeignKey(User, on_delete=models.CASCADE)
     likes = models.ManyToManyField(User, related_name='postlikes', blank=True)
     created_on = models.DateTimeField(auto_now_add=True)
     image = models.ImageField(blank=True, upload_to='media/incubators/posts')
-    post_type = models.CharField(max_length=10, choices=POST_TYPES, verbose_name="Contact Visibility")
+    post_type = models.CharField(
+        max_length=10, choices=POST_TYPES, verbose_name="Contact Visibility")
 
     def __str__(self):
         return self.title + '-' + self.incubator.name
@@ -390,7 +438,6 @@ class Comment(models.Model):
     added_by = models.ForeignKey(User, on_delete=models.CASCADE)
     created_on = models.DateTimeField(auto_now_add=True)
 
-
     def __str__(self):
         return self.value + '-' + self.added_by.username
 
@@ -402,6 +449,7 @@ class Comment(models.Model):
 class Room(models.Model):
     room_name = models.CharField(max_length=20, unique=True)
     links = models.ManyToManyField(User, through='Link')
+
     def __str__(self):
         return self.room_name
 
@@ -411,8 +459,10 @@ class Link(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     group = models.BooleanField(default=False)
     timestamp = models.DateTimeField(default=timezone.now, db_index=True)
+
     def __str__(self):
         return "%s [%s]" % (self.room.room_name, self.user.username)
+
 
 class Message(models.Model):
     links = models.ForeignKey(Link, on_delete=models.CASCADE)
